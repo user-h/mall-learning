d49736d0602f7623d016944557b9aacaac4abb6e
晚安 <1033078928@qq.com>
2022-06-01 00:10:29 +0800

    day1 学习封装思想，泛型编程（待学习）

---
+++ src/main/java/com/aihb/config/MyBatisConfig.java
@@ -0,0 +1,13 @@
+package com.aihb.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis配置类  暂时未使用
+ */
+//@Configuration
+//@MapperScan("com.aihb.mapper")
+public class MyBatisConfig {
+}
+
