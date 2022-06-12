ffb598f1a87464e3b6caa9e250f6362e0f48fd71
晚安 <1033078928@qq.com>
2022-06-12 16:55:53 +0800

    整合SpringSecurity和JWT实现认证和授权

---
+++ src/main/java/com/aihb/mapper/UmsPermissionMapper.java
@@ -0,0 +1,33 @@
+package com.aihb.mapper;
+
+import com.aihb.model.UmsPermission;
+import com.aihb.model.UmsPermissionExample;
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UmsPermissionMapper {
+    int countByExample(UmsPermissionExample example);
+
+    int deleteByExample(UmsPermissionExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(UmsPermission record);
+
+    int insertSelective(UmsPermission record);
+
+    List<UmsPermission> selectByExample(UmsPermissionExample example);
+
+    UmsPermission selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") UmsPermission record, @Param("example") UmsPermissionExample example);
+
+    int updateByExample(@Param("record") UmsPermission record, @Param("example") UmsPermissionExample example);
+
+    int updateByPrimaryKeySelective(UmsPermission record);
+
+    int updateByPrimaryKey(UmsPermission record);
+}
