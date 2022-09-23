a5cc8d0a28311dc7df5cab02da6a3d2f851cf0a6
晚安 <a1033078928@gmail.com>
2022-09-23 17:57:21 +0800

    整合Elasticsearch实现商品搜索

---
+++ src/main/java/com/aihb/mapper/EsProductDao.java
@@ -0,0 +1,13 @@
+package com.aihb.mapper;
+
+import com.aihb.nosql.elasticsearch.document.EsProduct;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * 搜索系统中的商品管理自定义Dao
+ */
+public interface EsProductDao {
+    List<EsProduct> getAllEsProductList(@Param("id") Long id);
+}
